6c7ae6b6e4a0f5d9b443d039a9ae4de59381ca8c
Nirav Malani <malnirav@gmail.com>
2014-04-24 13:17:01 -0400

    testthat fix

--- tests/runTests.R
+++ tests/runTests.R
@@ -1,6 +1,5 @@
 ## unit tests will not be done if testthat is not available
 if(require("testthat", quietly=TRUE)) {
-    test_dir("inst/unitTests/", reporter="minimal")
     library(hiAnnotator)    
     test_package("hiAnnotator")    
 } else {
